316490bba820fe1ba756efe86bf483bba8e07f5f
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-21 21:52:54 +0300

    5.3. Подготовка данных для главного экрана

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -31,4 +31,42 @@
     </v-row>
   </v-container>
 </template>
-  
+
+<script>
+export default {
+  data() {
+    return {
+      ads: [
+        {
+          title: "First",
+          desc: "First Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          id: "1"
+        },
+        {
+          title: "Second",
+          desc: "Second Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+          id: "2"
+        },
+        {
+          title: "Third",
+          desc: "Thitd Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+          id: "3"
+        },
+        {
+          title: "Fouth",
+          desc: "Fouth Desc",
+          promo: true,
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+          id: "4"
+        }
+      ]
+    }
+  }
+}
+</script>
