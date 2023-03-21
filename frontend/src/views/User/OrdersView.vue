cd8b1ff3d211f9a345255322fa6c4f17f1c849db
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-21 22:17:29 +0300

    6.3. Обработка клика на checkbox

--- frontend/src/views/User/OrdersView.vue
+++ frontend/src/views/User/OrdersView.vue
@@ -7,7 +7,7 @@
                     <v-list-item v-for="order in orders" :key="order.id">
                         <template>
                             <v-list-item-action>
-                                <v-checkbox :input-value="order.done" color="primary"></v-checkbox>
+                                <v-checkbox :input-value="order.done" color="primary" @click="markDone(order)"></v-checkbox>
                             </v-list-item-action>
                         </template>
                         <v-list-item-title>{{ order.name }}</v-list-item-title>
@@ -37,6 +37,12 @@ export default {
                     done: true
                 }
             ]
+        }
+    },
+    methods: {
+        markDone(order) {
+            order.done = !order.done
+            console.log(order.done)
         }
     }
 }
