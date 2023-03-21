b415a2f1647781f64b7808db1119920c4a48dfbe
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-21 21:17:56 +0300

    2.8. Подключение экранов к роутеру

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,19 @@
 <template>
-
   <v-app>
     <v-navigation-drawer app v-model="drawer">
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="title">КИПУ</v-list-item-title>
-          <v-list-item-subtitle>Учебный проект</v-list-item-subtitle>
+          <v-list-item-title class="title">
+            КИПУ
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Учебный проект
+          </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-divider></v-divider>
       <v-list dense>
-        <v-list-item v-for="link in links" :key="link.title">
-
+        <v-list-item v-for="link in links" :key="link.title" :to="link.url">
           <template v-slot:prepend>
             <v-icon :icon="link.icon"></v-icon>
           </template>
@@ -19,13 +21,11 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-
-
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn text v-for="link in links" :key="link.title">
+        <v-btn text v-for="link in links" :key="link.title" :to="link.url">
           <v-icon start :icon="link.icon"></v-icon>
           {{ link.title }}
         </v-btn>
@@ -38,7 +38,6 @@
 </template>
 
 
-  
 
 <script>
 export default {
