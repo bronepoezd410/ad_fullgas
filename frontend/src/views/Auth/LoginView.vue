70873a5099cd0a833760ebbc6363e72f8c63b5ca
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-21 21:26:29 +0300

    3.2. Добавление переменных для полей ввода

--- frontend/src/views/Auth/LoginView.vue
+++ frontend/src/views/Auth/LoginView.vue
@@ -8,9 +8,11 @@
                     </v-toolbar>
                     <v-card-text>
                         <v-form>
-                            <v-text-field prepend-icon="mdi-account" name="email" label="Email" type="email">
+                            <v-text-field prepend-icon="mdi-account" name="email" label="Email" type="email"
+                                v-model="email">
                             </v-text-field>
-                            <v-text-field prepend-icon="mdi-lock" name="password" label="Password" type="password">
+                            <v-text-field prepend-icon="mdi-lock" name="password" label="Password" type="password"
+                                v-model="password">
                             </v-text-field>
                         </v-form>
                     </v-card-text>
@@ -27,8 +29,9 @@
 export default {
     data() {
         return {
+            email: "",
+            password: ""
         }
     }
 }
 </script>
-    
