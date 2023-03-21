f55b238c9c2cfffe025e5d2ce8ff5251c0b8fc07
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-21 22:08:33 +0300

    6.2. Подготовка и подключение данных для экрана заказов

--- frontend/src/views/User/OrdersView.vue
+++ frontend/src/views/User/OrdersView.vue
@@ -4,18 +4,18 @@
             <v-col cols="6" offset="3">
                 <h1 class="text--secondary mb-3 mt-3">Orders</h1>
                 <v-list subheader two-line flat>
-                    <v-list-item>
+                    <v-list-item v-for="order in orders" :key="order.id">
                         <template>
                             <v-list-item-action>
-                                <v-checkbox color="primary" ></v-checkbox>
+                                <v-checkbox :input-value="order.done" color="primary"></v-checkbox>
                             </v-list-item-action>
                         </template>
-                        <v-list-item-title>Notifications</v-list-item-title>
-                        <v-list-item-subtitle>Allow notifications</v-list-item-subtitle>
+                        <v-list-item-title>{{ order.name }}</v-list-item-title>
+                        <v-list-item-subtitle>>{{ order.phone }}</v-list-item-subtitle>
                         <template v-slot:append>
-                                <v-list-item-action>
-    		<v-btn class="primary">Open</v-btn>
-        </v-list-item-action>
+                            <v-list-item-action>
+                                <v-btn :to="'/ad/' + order.adId" class="primary">Open</v-btn>
+                            </v-list-item-action>
                         </template>
                     </v-list-item>
                 </v-list>
@@ -23,3 +23,21 @@
         </v-row>
     </v-container>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            orders: [
+                {
+                    id: "123",
+                    name: "Kostya",
+                    phone: "+7(978)000-00-05",
+                    adId: "1",
+                    done: true
+                }
+            ]
+        }
+    }
+}
+</script>
