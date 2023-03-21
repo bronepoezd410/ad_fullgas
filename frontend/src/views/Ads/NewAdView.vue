0d974b447f7c51e5dc15bb931b0449b99491457c
Demiurg <danyushis.m.i.21@gmail.com>
2023-03-21 22:20:30 +0300

    7.1. Верстка формы

--- frontend/src/views/Ads/NewAdView.vue
+++ frontend/src/views/Ads/NewAdView.vue
@@ -1,16 +1,28 @@
 <template>
     <v-container>
-      <v-row justify="center">
-        <h1>NewAdView</h1>
-      </v-row>
+        <v-row>
+            <v-col cols="8" offset="2">
+                <h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
+                <v-form v-model="valid" ref="form" validation>
+                    <v-text-field name="title" label="Ad Title" type="text" v-model="title"
+                        :rules="[v => !!v || 'Title is required']">
+                    </v-text-field>
+                    <v-textarea name="description" label="Ad Description" type="text" v-model="description"
+                        :rules="[v => !!v || 'Description is required']" class="mb-3"></v-textarea>
+                </v-form>
+            </v-col>
+        </v-row>
     </v-container>
-  </template>
-  <script>
-  export default {
+</template>
+<script>
+export default {
     data() {
-      return {
-      }
-    }
-  }
-  </script>
-    
+        return {
+            valid: false,
+            title: "",
+            description: "",
+            promo: false,
+        };
+    },
+};
+</script>
