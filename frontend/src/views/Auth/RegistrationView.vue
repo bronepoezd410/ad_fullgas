91292b7d2289edbcf8e3bfe2e8165950c5224ba2
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-03 14:49:43 +0300

    Отправка action для регистрации пользователя

--- frontend/src/views/Auth/RegistrationView.vue
+++ frontend/src/views/Auth/RegistrationView.vue
@@ -57,7 +57,7 @@ export default {
                     email: this.email,
                     password: this.password
                 }
-                console.log(user)
+                this.$store.dispatch('registerUser', user)
             }
         }
     }
