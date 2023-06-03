d5a7bd0ce8941c0db106aa6e18d170152adbd2eb
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-03 15:02:19 +0300

    Редирект после успешного создания пользователя

--- frontend/src/views/Auth/RegistrationView.vue
+++ frontend/src/views/Auth/RegistrationView.vue
@@ -64,6 +64,12 @@ export default {
                     password: this.password
                 }
                 this.$store.dispatch('registerUser', user)
+                .then(() => {
+                        this.$router.push("/")
+                    })
+                    .catch((err) => {
+                        console.log(err.message)
+                    })
             }
         }
     }
