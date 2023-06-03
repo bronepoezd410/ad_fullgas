d3962e922d0e644163178058779fccdfb7e1dca5
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-03 15:59:05 +0300

    Управление кнопкой Edit

--- frontend/src/views/Ads/EditAdModal.vue
+++ frontend/src/views/Ads/EditAdModal.vue
@@ -61,6 +61,9 @@ export default {
                 })
                 this.modal = false
             }
+        },
+        isOwner() {
+            return this.ad.ownerId === this.$store.getters.user.id
         }
     }
 }
