5b15900deceb5a8581159089b8be0deff13a50f4
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-03 15:44:28 +0300

    Подключение диалогового окна

--- frontend/src/views/Ads/AdView.vue
+++ frontend/src/views/Ads/AdView.vue
@@ -1,44 +1,38 @@
 <template>
    <v-container>
-   <v-row>
-   <v-col cols="12">
-   <v-card class="mt-5">
-   <v-img
-   height="400px"
-   :src="ad.src"
-   cover
-   ></v-img>
-   <v-card-text>
-   <h1 class="text--primary mb-3">{{ ad.title }}</h1>
-   <p>
-   {{ ad.desc}}
-   </p>
-   </v-card-text>
-   <v-card-actions>
-   <v-spacer></v-spacer>
-   <v-btn class="warning" color="orange">Edit</v-btn>
-   <v-btn class="success" color="green">Buy</v-btn>
-   </v-card-actions>
-   </v-card>
-   </v-col>
-   </v-row>
+       <v-row>
+           <v-col cols="12">
+               <v-card class="mt-5">
+                   <v-img height="300px" :src="ad.src"></v-img>
+                   <v-card-text>
+                       <h1 class="text--primary mb-3">{{ ad.title }}</h1>
+                       <p>
+                           {{ ad.desc }}
+                       </p>
+                   </v-card-text>
+                   <v-card-actions>
+                       <v-spacer></v-spacer>
+                       <modal-dialog></modal-dialog>
+                       <v-btn class="success" color="green">Buy</v-btn>
+                   </v-card-actions>
+               </v-card>
+           </v-col>
+       </v-row>
    </v-container>
-   </template>
-   
-   
-   
-   <script>
-   export default {
-   data() {
-   return {};
-   },
+</template>
+<script>
+import EditAdModal from '../EditAdModal'
+export default {
    props: ['id'],
    computed: {
-   ad() {
-   const id = this.id
-   return this.$store.getters.adById(id)
+       ad() {
+           const id = this.id
+           return this.$store.getters.adById(id)
+       }
+   },
+   components: {
+       'modal-dialog': EditAdModal
    }
-   }
-   };
-   </script>
-   
+
+};
+</script>
