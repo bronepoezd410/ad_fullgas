963f79f60804713abab356291ea4dddfd148c60e
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-03 15:35:08 +0300

    Логика отображений объявлений пользователя

--- frontend/src/store/ads.js
+++ frontend/src/store/ads.js
@@ -76,9 +76,11 @@ export default {
                     return ad.promo
                 })
             },
-            myAds(state) {
-                return state.ads
-        },
+            myAds(state, getters) {
+    return state.ads.filter(ad => {
+    return ad.userId == getters.user.id
+    })
+    },
         adById(state) {
         return id => {
         return state.ads.find(ad => ad.id == id)
