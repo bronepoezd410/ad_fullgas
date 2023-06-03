d402a93b9411aa74e9d6652f1a461a180e8a6207
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-03 16:16:27 +0300

    Создание store заказов

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,10 +2,11 @@ import { createStore } from 'vuex'
 import ads from './ads'
 import user from './user'
 import shared from './shared'
+import order from './orders'
 
 const store = createStore({
 modules:{
-    ads, user, shared
+    ads, user, shared, order
 }
 })
 
