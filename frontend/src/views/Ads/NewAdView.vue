130e6c0bfed58c53161e8a39d2671eea814e8cfb
Demiurg <danyushis.m.i.21@gmail.com>
2023-06-03 15:30:44 +0300

    Рефакторинг метода createAd

--- frontend/src/views/Ads/NewAdView.vue
+++ frontend/src/views/Ads/NewAdView.vue
@@ -31,7 +31,12 @@
                <v-row>
                    <v-col cols="8">
                        <v-spacer></v-spacer>
-                       <v-btn color="success" @click="createAd">Create Ad</v-btn>
+                       <v-btn
+   color="success"
+   @click="createAd"
+   :loading="loading"
+   :disabled:="!valid || loading"
+   >Create Ad</v-btn>
                    </v-col>
                </v-row>
            </v-col>
@@ -60,6 +65,11 @@ export default {
                this.$store.dispatch("createAd", ad)
            }
        },
+   },
+   computed: {
+       loading() {
+           return this.$store.getters.loading
+       }
    },
 };
 </script>
